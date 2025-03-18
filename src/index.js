ab49bb63f9d46f6fc66eb462cba845f16c9761a0
Ethan Tran <khoitranec@gmail.com>
2025-03-19 01:10:19 +0700

    Update: add jest, setting jest config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './style.css';
+
+export function add(a, b) {
+  return a + b;
+}
+
+console.log('duoc ban');
---
+++ src/index.test.js
@@ -0,0 +1,5 @@
+import { add } from './index';
+
+test('aefs', () => {
+  expect(add(4, 8)).toBe(12);
+});
